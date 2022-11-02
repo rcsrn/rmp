4b3ac8956fba83d9e3e9c704213f09b1ab0f9f17
nsgbanks@ciencias.unam.mx <nsgbanks@ciencias.unam.mx>
2022-11-02 00:54:54 -0600

    Skeletons added in miner.go

--- pkg/miner/miner.go
+++ pkg/miner/miner.go
@@ -1,1 +1,22 @@
 package miner
+
+import (
+	"github.com/rcsrn/rmp/pkg/database"
+)
+
+type Miner struct {
+	filePaths []string
+	songs []*database.Rola
+}
+
+func createNewMiner() *Miner {
+	return Miner{make([]string, 0), make([]*Rolas, 0)}
+}
+
+func(miner *Miner) Traverse() {
+	
+}
+
+func(miner *Miner) MineTags() {
+	
+}
