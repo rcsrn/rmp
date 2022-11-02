63e99a19d0cb54be222ee99b84d4b81c0e306ff0
nsgbanks@ciencias.unam.mx <nsgbanks@ciencias.unam.mx>
2022-11-02 00:51:26 -0600

    skeletons in database added

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,2 +1,33 @@
 package database
 
+import (
+	"database/sql"
+)
+
+type DataBase struct {
+	db *sql.DB
+}
+
+func CreateNewDataBase() *DataBase {
+	return nil
+}
+
+func (database *DataBase) AddRola() {
+	
+}
+
+func (database *DataBase) AddAlbum() {
+	
+}
+
+func (database *DataBase) AddPerformer() {
+	
+}
+
+func (database *DataBase) AddGroup() {
+	
+}
+
+func (database *DataBase) AddPerson() {
+	
+}
---
+++ pkg/database/rola.go
@@ -0,0 +1,90 @@
+package database
+
+type Rola struct {
+	id int64
+	performer string
+	album string
+	path string
+	title string
+	track int
+	year int
+	genre string
+}
+
+func createNewRola() *Rola {
+	return &Rola{
+		id: 0,
+		performer: "Unknown",
+		album: "Unknown",
+		path: "Unknown",
+		title: "Unknown",
+		track: 0,
+		year: 0,
+		genre: "Unknown",
+	}
+}
+
+func (rola *Rola) GetID() int64 {
+	return rola.id
+}
+
+func (rola *Rola) GetPerformer() string {
+	return rola.performer
+}
+
+func (rola *Rola) GetAlbum() string {
+	return rola.album
+}
+
+func (rola *Rola) GetPath() string {
+	return rola.path
+}
+
+func (rola *Rola) GetTitle() string {
+	return rola.title
+}
+
+func (rola *Rola) GetTrack() int {
+	return rola.track
+}
+
+func (rola *Rola) GetYear() int {
+	return rola.year
+}
+
+func (rola *Rola) GetGenre() string {
+	return rola.genre
+}
+
+func (rola *Rola) SetArtist(artist string) {
+	rola.performer = strings.TrimSpace(performer)
+}
+
+func (rola *Rola) SetTitle(title string) {
+	rola.title = strings.TrimSpace(title)
+}
+
+func (rola *Rola) SetAlbum(album string) {
+	rola.album = strings.TrimSpace(album)
+}
+
+func (rola *Rola) SetTrack(track int) {
+	rola.track = track
+}
+
+func (rola *Rola) SetYear(year int) {
+	rola.year = year
+}
+
+func (rola *Rola) SetGenre(genre string) {
+	rola.genre = strings.TrimSpace(genre)
+}
+
+func (rola *Rola) SetPath(path string) {
+	rola.path = strings.TrimSpace(path)
+}
+
+func (rola *Rola) SetID(id int64) {
+	rola.id = id
+}
+
